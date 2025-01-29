1f9dab6bd978846432435e2635465a418018b9f7
Yossab_Samuel <yossabpro5@gmail.com>
2025-01-29 08:47:30 +0200

    edit validations files

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 
 use src\support\ArrayWrapper;
+use src\validation\rules\RequireRule;
+use src\validation\Validator;
 
 require_once "src/support/helpers.php";
 require_once base_path() . "vendor/autoload.php";
 require_once base_path() . "routes/web.php";
 
 app()->run();
+
+$validator = new Validator();
+
+$validator->setRules([
+  'username' => ['required', 'between:5, 10'
+]]);
+
+$validator->make([
+  'username' => 'yossabbbbbbbbb',
+]);
+
+echo '<pre>';
+var_dump($validator->errors());
+echo '</pre>';;
---
+++ src/validation/ErrorBag.php
@@ -0,0 +1,17 @@
+<?php
+namespace src\validation;
+
+class ErrorBag
+{
+  protected array $errors = [];
+  public function add($key, $message) 
+  {
+    $this->errors[$key][] = $message;
+  }
+  public function __get($key)
+  {
+    if( property_exists($this, $key)) {
+      return $this->$key;
+    }
+  }
+}
